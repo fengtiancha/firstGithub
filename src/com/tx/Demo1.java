d651ba5b5e605566703b768d315151b0d9fe0ffe
zhaolei <zhaolei@tianxinweb.com>
2016-06-16 15:54:16 +0800

    修改demo1和demo2的值

--- src/com/tx/Demo1.java
+++ src/com/tx/Demo1.java
@@ -3,5 +3,6 @@ package com.tx;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Demo1");
+		System.out.println("在分支test1上面的测试");
 	}
 }
--- src/com/tx/Demo2.java
+++ src/com/tx/Demo2.java
@@ -3,5 +3,6 @@ package com.tx;
 public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("Demo2");
+		System.out.println("在分支test1上面的测试");
 	}
 }
