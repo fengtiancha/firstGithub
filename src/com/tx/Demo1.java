da23c54868032d4ce7709d58318ac76a29e8f03f
zhaolei <zhaolei@tianxinweb.com>
2016-06-16 15:17:20 +0800

    赵垒是测试git的提交

---
+++ src/com/tx/Demo1.java
@@ -0,0 +1,7 @@
+package com.tx;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("github1");
+	}
+}
---
+++ src/com/tx/github2.java
@@ -0,0 +1,7 @@
+package com.tx;
+
+public class github2 {
+	public static void main(String[] args) {
+		System.out.println("github2");
+	}
+}
